4627c393feeb8b7bb5f46d7e4a4f61452855d1cb
Eduardo Almeida <duduf2005@hotmail.com>
2022-03-29 21:05:03 -0300

    correção TS no babel webpack

--- reactjs/01-github-explorer/babel.config.js
+++ reactjs/01-github-explorer/babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    "@babel/preset-typescript",
     ['@babel/preset-react', {
       runtime: 'automatic' //Precisa disso para executar sem importar o React no arquivo
-    }]
+    }],
 
   ]
 };
