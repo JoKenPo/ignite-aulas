6339ae073a97de25446c0ea5810328e252f0f3be
Eduardo Almeida <duduf2005@hotmail.com>
2022-04-12 23:36:06 -0300

    feat: Componentização

--- reactjs/02-dt-money/src/App.tsx
+++ reactjs/02-dt-money/src/App.tsx
@@ -1,9 +1,12 @@
+import { Header } from "./components/Header/index";
+import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
     </div>
   );
