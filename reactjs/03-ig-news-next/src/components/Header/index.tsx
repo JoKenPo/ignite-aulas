4f53d0aa2afa44d9578a7a54f1e130dafe51dc5b
Eduardo Almeida <eduardo.almeida@spread.com.br>
2022-06-29 09:57:08 -0300

    feat: cms prismic + header

--- reactjs/03-ig-news-next/src/components/Header/index.tsx
+++ reactjs/03-ig-news-next/src/components/Header/index.tsx
@@ -1,15 +1,23 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SignInButton } from '../SingInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from './../ActiveLink/index';
 
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} >Home</a>
-          <a >Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- reactjs/03-ig-news-next/src/pages/posts/index.tsx
+++ reactjs/03-ig-news-next/src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import styles from './styles.module.scss'
 import { getPrismicClient } from './../../services/prismic';
 import { GetStaticProps } from 'next';
 import * as Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+type Post = {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,17 +26,14 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>23 de Junho de 2022</time>
-            <strong>Creating a Monorepo with</strong>
-            <p>Texto grandãoo asdiuajsdiuashdiusadh asiduahsidu asdiuashndiasndas</p>
-          </a>
+          {posts?.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
 
-          <a href="#">
-            <time>23 de Junho de 2022</time>
-            <strong>Creating a Monorepo with</strong>
-            <p>Texto grandãoo asdiuajsdiuashdiusadh asiduahsidu asdiuashndiasndas</p>
-          </a>
+          ))}
 
           <a href="#">
             <time>23 de Junho de 2022</time>
@@ -48,9 +57,23 @@ export const getStaticProps: GetStaticProps = async () => {
       pageSize: 100,
     })
 
-  console.log('response: ', JSON.stringify(response, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      // title: RichText.asText(post.data.Title),
+      title: post.data.Title,
+      excerpt: post.data.Content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
